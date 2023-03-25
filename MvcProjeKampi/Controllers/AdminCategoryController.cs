aaf75b7792fe280b95c46efa1ad0f5208d112076
furkaydin <furkay16@gmail.com>
2023-03-25 03:27:35 +0300

    adminlte kuruldu.sidebar duzenlendi.

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var result = cm.GetList();
+            return View(result);
+        }
+    }
+}
