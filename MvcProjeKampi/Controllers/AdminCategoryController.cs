dae43a886a5b6c9d1f6ab4dfaa50a8941c2fe82a
furkaydin <furkay16@gmail.com>
2023-03-26 16:43:19 +0300

    kategori add

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,34 @@ namespace MvcProjeKampi.Controllers
         {
             var result = cm.GetList();
             return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAdd(p);
+            CategoryValidatior categoryValidatior = new CategoryValidatior(); // validator nesnesi oluşturuldu.
+            ValidationResult results = categoryValidatior.Validate(p); // parametreden gelen p yi categoryValidatior nesnesindeki değerleri validate et.
+            if (results.IsValid)
+            {
+                cm.CategoryAddBL(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View(); // ekleme işlemi gerçekleştikten sonra "" içindeki metoda yönlendir.
         }
     }
 }
